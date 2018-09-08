829ffb057c9bc21c8414eb08d2fbf8351ad7198e
Joseph McGroarty <joeymcgroarty@gmail.com>
2018-09-07 21:25:20 -0400

    chapter 3 exercises variables

---
+++ methods/exercise1.rb
@@ -0,0 +1,15 @@
+# exercise 1
+def greeting(name)
+  puts "Hello #{name}"
+end
+
+greeting("Joseph")
+
+=begin 
+exercise 2:
+1. => 2
+2. nil
+3. "Joe"
+4. "four"
+5. nil
+=end
---
+++ methods/exercise4.rb
@@ -0,0 +1,16 @@
+#exercise 4: it won't print anything to the screen.
+
+#exercise 5:
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+# puts "Yippeee!!!!"
+
+=begin 
+exercise 6:
+the user called an argument that takes two methods but only included one.
+=end
