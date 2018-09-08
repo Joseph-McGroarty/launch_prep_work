b71c5774974dad09d22e5def473a0ff27aac805f
Joseph McGroarty <joeymcgroarty@gmail.com>
2018-09-07 21:04:39 -0400

    chapter 2 exercises variables

---
+++ variables/name.rb
@@ -0,0 +1,21 @@
+#exercise 1
+puts "type your name"
+name = gets.chomp
+puts "Hello #{name}."
+
+#exercise 3
+10.times do |i|
+  puts name
+end
+
+#exercise 4
+puts "what's your first name?"
+first = gets.chomp
+puts "and your last name?"
+last = gets.chomp
+puts "Nice to meet you, #{first} #{last}."
+
+=begin 
+exercise 5:
+program 1 puts "3" as x. program 2 returns an error because x isn't defined outside of the block.
+=end
